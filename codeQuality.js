36add8d6b0a67a6797f5c94bc986e0993a93ef9c
Sridharam Srikanth <59954461+thesrisri@users.noreply.github.com>
2022-02-12 15:08:20 +0530

    Update codeQuality.js

--- codeQuality.js
+++ codeQuality.js
@@ -4,7 +4,7 @@
 const getUsers = async () => {
   let doc = await Promise.all([getUser(), getProfile(), getPosts()]);
   let [user, profile, p] = doc;
-  return ({
+  return (userProfile :{
     user,
     profile,
     posts: p,
